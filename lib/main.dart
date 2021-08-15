9998eb159a47759df9fda0863b878cf6b7bfd716
alex <a99som@yandex.ru>
2021-08-15 23:20:35 +0300

    config left column

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _counter++;
     });
-    if(obscure_password_text){
+    if (obscure_password_text) {
       obscure_password_text = false;
     }
   }
@@ -44,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _decrementCounter() {
     setState(() {
       _counter--;
-      if(!obscure_password_text){
+      if (!obscure_password_text) {
         obscure_password_text = true;
       }
     });
@@ -53,80 +54,141 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 30,
-                    width: 240,
-                    child: ElevatedButton(
-                        onPressed: _incrementCounter,
-                        child: const Text("Прибавить")),
+      backgroundColor: const Color.fromARGB(255, 229, 229, 229),
+      body: Center(
+        child: Container(
+          width: 1280,
+          height: 780,
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 720,
+                height: 780,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 78, 0, 50),
+                      child: Text(
+                        'Вход в учётную запись',
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 32,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
+                      child: Container(
+                        height: 60,
+                        width: 480,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 5.0, // soften the shadow
+                              spreadRadius: 5.0, //extend the shadow
+                            )
+                          ],
+                        ),
+                        child: const Align(
+                          alignment: Alignment.center,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: 'Email',
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 25),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      child: Container(
+                        height: 60,
+                        width: 480,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 5.0, // soften the shadow
+                              spreadRadius: 5.0, //extend the shadow
+                            )
+                          ],
+                        ),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: TextField(
+                            obscureText: obscure_password_text,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              labelText: 'Password',
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 25),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 266, 0, 0),
+                      child: SizedBox(
+                        height: 60,
+                        width: 330,
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              // side:
+                              //     BorderSide(color: Colors.red)
+                            ))),
+                            onPressed: _incrementCounter,
+                            child: const Text("Прибавить")),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 560,
+                height: 780,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 408, 0, 0),
+                    child: SizedBox(
+                      height: 60,
+                      width: 330,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            // side:
+                            //     BorderSide(color: Colors.red)
+                          ))),
+                          onPressed: _decrementCounter,
+                          child: const Text("Убавить")),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  height: 30,
-                  width: 240,
-                  child: ElevatedButton(
-                      onPressed: _decrementCounter,
-                      child: const Text("Убавить")),
-                ),
+                ]),
               ),
-            ]),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "$_counter",
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: SizedBox(
-            height: 60,
-            width: 480,
-            child: TextFormField(
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                labelText: 'Email',
-              ),
-            ),
+            ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: SizedBox(
-            height: 60,
-            width: 480,
-            child: TextFormField(
-              key: const Key("password"),
-              obscureText: obscure_password_text,
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                labelText: 'Password',
-              ),
-            ),
-          ),
-        ),
-      ]),
+      ),
     );
   }
 }
